97f20a736edf613788e39ba35ba79041a8b1688a
MD. MOINUL ISLAM <118617222+MOINUL909@users.noreply.github.com>
2023-11-10 11:26:30 +0600

    Add files via upload

---
+++ 1006.c
@@ -0,0 +1,20 @@
+/*MOINUL*/
+#include <stdio.h>
+
+int main() {
+    double A,B,C,MEDIA;
+    scanf("%lf%lf%lf",&A,&B,&C);
+    A=A*2;
+    B=B*3;
+    C=C*5;
+    MEDIA=(A+B+C)/10;
+    printf("MEDIA = %.1lf\n",MEDIA);
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+    return 0;
+}
